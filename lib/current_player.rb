98a6364b4294b5c233a19b914722411707223f60
Benjamin Krigel <benjamin.krigel@powerhrg.com>
2018-05-01 23:00:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,9 @@ def turn_count(board)
   counter
 end
 
-def current_player
-
+def current_player(board)
+  if turn_count(board) % 2 == 1
+    puts "X"
+  else turn_count(board) % 2 == 0
+    puts "O"
 end
