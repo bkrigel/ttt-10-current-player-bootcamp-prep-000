56db3b0b2ee1f78a11260d4fefdc98d8d1a2b59c
Benjamin Krigel <benjamin.krigel@powerhrg.com>
2018-05-01 23:08:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  counter = 1
+  counter = 0
   board.each do |space|
     if space == "X" || space == "O"
       counter += 1
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 1
+  if turn_count(board) % 2 == 0
     "X"
-  else turn_count(board) % 2 == 0
+  else turn_count(board) % 2 == 1
     "O"
   end
 end
